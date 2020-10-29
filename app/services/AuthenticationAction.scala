6958d8b50eef1e94f158cd63045f39c0ee126f43
Bao Khanh Dinh <khanhdb@sandinh.net>
2020-10-29 10:07:01 +0700

    authenticationAction

---
+++ app/services/AuthenticationAction.scala
@@ -0,0 +1,24 @@
+package services
+
+import javax.inject.Inject
+import play.api.Logger
+import play.api.mvc.Results.Unauthorized
+import play.api.mvc.{ActionBuilderImpl, BodyParsers, Request, Result}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AuthenticationAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends ActionBuilderImpl(parser){
+
+  private val logger: Logger = Logger(this.getClass)
+
+    override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] = {
+      request.session.get("connected") match {
+        case None =>
+          Future.successful(Unauthorized)
+        case Some(email) =>
+          logger.debug(s"$email passed authentication")
+          block(request)
+      }
+    }
+}
