92a4ec9c3b6a9c2cb3e84a775937a7177ce23c5d
Bao Khanh Dinh <khanhdb@sandinh.net>
2020-11-24 11:04:50 +0700

    refactor

--- app/controllers/NotificationController.scala
+++ app/controllers/NotificationController.scala
@@ -1,19 +1,15 @@
 package controllers
 
-import java.util.NoSuchElementException
-
 import javax.inject._
 import models.{DatabaseExecutionContext, Notification, NotificationRepository}
 import play.api.Logger
-import play.api.libs.json.{JsObject, JsValue}
+import play.api.libs.json.JsValue
 import play.api.mvc._
 import services.AuthenticationActionBuilder
 
-import scala.concurrent.Future
-
 
 @Singleton
-class NotificationController @Inject()(repository : NotificationRepository, auth : AuthenticationActionBuilder, cc: ControllerComponents, implicit val databaseExecutionContext: DatabaseExecutionContext) extends AbstractController(cc) {
+class NotificationController @Inject()(repository : NotificationRepository, auth : AuthenticationActionBuilder, cc: ControllerComponents, implicit val databaseExecutionContext: DatabaseExecutionContext) extends AbstractController(cc) with JsonBodyHandler {
   private val logger: Logger = Logger(this.getClass)
 
   def notifications: Action[AnyContent] = auth.async{ implicit request =>
@@ -28,18 +24,11 @@ class NotificationController @Inject()(repository : NotificationRepository, auth
     repository.changeStatus(email, id).map(notUpdated => if (notUpdated) InternalServerError else Ok)
   }
 
-  def create : Action[JsValue] = auth(parse.json).async{ implicit request =>
-    request.body match {
-       case JsObject(underlying) =>
-             try {
-               val subject = underlying("subject")
-               val notifyTo = underlying("notify_to")
-               repository.create(subject.as[String], notifyTo.as[String]).map(notUpdated => if (notUpdated) InternalServerError else Created)
-             } catch {
-               case _ : NoSuchElementException =>
-                 Future.successful(BadRequest("wrong format"))
-             }
-       case _ => Future.successful(BadRequest("wrong format"))
+  def create : Action[JsValue] = auth(parse.json).async { implicit request =>
+    handleJsObject { mapValues =>
+      val subject = mapValues("subject")
+      val notifyTo = mapValues("notify_to")
+      repository.create(subject.as[String], notifyTo.as[String]).map(notUpdated => if (notUpdated) InternalServerError else Created)
     }
   }
 }
