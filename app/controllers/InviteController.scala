de254d7e1aad936cf4efa8e4bf2fe6ed11a31728
Bao Khanh Dinh <khanhdb@sandinh.net>
2020-11-24 11:07:12 +0700

    handle json object

--- app/controllers/InviteController.scala
+++ app/controllers/InviteController.scala
@@ -2,40 +2,30 @@ package controllers
 
 import javax.inject._
 import models.{DatabaseExecutionContext, Invite, InviteRepository}
-import play.api.libs.json.{JsObject, JsValue}
+import play.api.libs.json.JsValue
 import play.api.mvc._
 import services.AuthenticationActionBuilder
 
-import scala.concurrent.Future
-
 
 @Singleton
-class InviteController @Inject()(repository : InviteRepository, auth : AuthenticationActionBuilder, cc: ControllerComponents, implicit val databaseExecutionContext: DatabaseExecutionContext) extends AbstractController(cc) {
+class InviteController @Inject()(repository: InviteRepository, auth: AuthenticationActionBuilder, cc: ControllerComponents, implicit val databaseExecutionContext: DatabaseExecutionContext) extends AbstractController(cc) with JsonBodyHandler {
 
-  def invites: Action[AnyContent] = auth.async{ implicit request =>
+  def invites: Action[AnyContent] = auth.async { implicit request =>
     val email = request.session("email")
-    repository.invites(email).map{invites =>
+    repository.invites(email).map { invites =>
       Ok(Invite.toJson(invites))
     }
   }
 
-  def update : Action[JsValue] = auth(parse.json).async{ implicit request =>
+  def update: Action[JsValue] = auth(parse.json).async { implicit request =>
     val email = request.session("email")
-    request.body match {
-      case JsObject(map) =>
-        try {
-          val id = map("id").as[Int]
-          val topicId = map("topicId").as[Int]
-          val status = map("status").as[String]
-          repository.updateStatus(id, topicId, email, status).map{ notUpdated =>
-            if (notUpdated) InternalServerError("failed to update") else Ok
-          }
-        } catch {
-          case _ : NoSuchElementException =>
-            Future.successful(BadRequest("wrong data format"))
-        }
-      case _ =>
-        Future.successful(BadRequest("wrong data format"))
+    handleJsObject { map =>
+      val id = map("id").as[Int]
+      val topicId = map("topicId").as[Int]
+      val status = map("status").as[String]
+      repository.updateStatus(id, topicId, email, status).map { notUpdated =>
+        if (notUpdated) InternalServerError("failed to update") else Ok
+      }
     }
   }
 }
